4a0f5a20e9078f4adced03b26bcf0e92635ac356
ArnavSurana <arnav.surana051175@gmail.com>
2025-04-10 21:23:47 +0530

    k

--- src/appwrite/config.js
+++ src/appwrite/config.js
@@ -24,7 +24,6 @@ export class Service{
                     featuredImage,
                     status,
                     userid: userId, 
-                     // Changed to match database schema
                 }
             )
         }
